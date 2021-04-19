d648b0c2cd9e87cb52666025b173eeae96a481cb
EDWARD <60771749+KiritoEdward@users.noreply.github.com>
2021-04-19 10:18:21 +0700

    Add files via upload

---
+++ Tugas3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int A = 10, B = 20;
+	cout << "Nilai Variabel A : " << A << endl;
+	cout << "Nilai Variabel B : " << B << endl;
+	cout << "Hasil Perkalian Dua Variabel Adalah : " << A*B << endl;
+	return 0;
+}
